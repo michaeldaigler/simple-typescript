b526a2049e513300844808025352a6f13e0349df
Michael Daigler <mdaigler2601@gmail.com>
2021-04-09 20:03:58 -0400

    Mapped Types

---
+++ src/mapped-types.ts
@@ -0,0 +1,28 @@
+type Properties = 'propA' | 'propB';
+
+
+type MappedType<T> = {
+    [P in keyof T]: T[P] | null;
+}
+
+
+type MyNewType = MappedType<{ a: 'a', b: 'b' }>
+
+type Pick1<T, Properties extends keyof T> = {
+    [P in Properties]: T[P];
+}
+
+type MyNewType2 = Pick1<{ a: 'a', b: 'b' }, 'a'>;
+
+type Record1<K extends keyof any, T> = {
+    [P in K]: T;
+}
+
+const someRecord: Record1<string, number> = {}
+someRecord.apples = 10;
+someRecord.oranges = 10;
+
+
+interface Record2 {
+    [key: string]: number;
+}
